6b17670392847ecc48283105dea17b5387139aea
LutherMGP <mgrosenp@outlook.dk>
2023-11-22 19:37:45 +0100

    Update CompanyController.java

--- src/main/java/com/example/data22aexamprojectlnl2/controllers/CompanyController.java
+++ src/main/java/com/example/data22aexamprojectlnl2/controllers/CompanyController.java
@@ -1,7 +1,54 @@
 package com.example.data22aexamprojectlnl2.controllers;
 
-public class CompanyController
-{
+import com.example.data22aexamprojectlnl2.models.Company;
+import com.example.data22aexamprojectlnl2.services.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
+@RestController
+@RequestMapping("/companies")
+public class CompanyController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @PostMapping("/new")
+    public ResponseEntity<Company> addCompany(@RequestBody Company company) {
+        return ResponseEntity.ok(companyService.saveCompany(company));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Company>> getAllCompanies() {
+        return ResponseEntity.ok(companyService.getAllCompanies());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable int id) {
+        Company company = companyService.getCompanyById(id);
+        if (company != null) {
+            return ResponseEntity.ok(company);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Company> updateCompany(@RequestBody Company company, @PathVariable int id) {
+        Company updatedCompany = companyService.updateCompany(company, id);
+        if (updatedCompany != null) {
+            return ResponseEntity.ok(updatedCompany);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCompany(@PathVariable int id) {
+        companyService.deleteCompany(id);
+        return ResponseEntity.ok().build();
+    }
 }
+
